b9bf4d3ce8a0c3c0a05a56467a98d7f91cd7d9fa
Maxim Starikov <jouzen95@gmail.com>
2024-03-01 14:41:54 +0300

    added pojoModels for register test

---
+++ src/test/java/in/reqres/models/RegisterResponseModel.java
@@ -0,0 +1,25 @@
+package in.reqres.models;
+
+public class RegisterResponseModel {
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    String token;
+    Integer id;
+
+
+}
+
--- src/test/java/in/reqres/tests/HomeWorkTests.java
+++ src/test/java/in/reqres/tests/HomeWorkTests.java
@@ -1,18 +1,23 @@
-package in.reqres;
+package in.reqres.tests;
 
+import in.reqres.models.RegisterBodyModel;
+import in.reqres.models.RegisterResponseModel;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomeWorkTests {
 
     @Test
     void registerSuccessful () {
-        String authData = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+        RegisterBodyModel authData = new RegisterBodyModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("pistol");
 
-                given()
+        RegisterResponseModel registerResponse = given()
                 .log().all()
                 .contentType(JSON)
                 .body(authData)
@@ -21,7 +26,11 @@ public class HomeWorkTests {
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .extract().as(RegisterResponseModel.class);
+
+        assertEquals(4, registerResponse.getId());
+
+//                .body("token", is("QpwL5tke4Pnpja7X4"))
     }
 
     @Test
@@ -53,5 +62,33 @@ public class HomeWorkTests {
                         .log().all()
                         .statusCode(400)
                         .body("error", is("Missing password"));
+    }
+    @Test
+    void loginSuccessful () {
+        String authData = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+    @Test
+    void loginMissingEmail () {
+        String authData = "{ \"email\": \"\", \"password\": \"cityslicka\"}";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
     }
 }
